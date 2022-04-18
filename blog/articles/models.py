97285b68ff9a148613475650ca6c369a8bfd31b1
Vitaly Bogryakov <vitjrbog@icloud.com>
2022-04-18 09:17:48 +0500

    Update: changed type of field "date" in models "Article" and "Comment"

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField('Заголовок', max_length=50)
     definition = models.CharField('Описание', max_length=140)
     text = models.TextField('Текст')
-    date = models.IntegerField('Дата публикации')
+    date = models.DecimalField(
+        'Дата публикации', max_digits=15, decimal_places=0)
 
     def __str__(self) -> str:
         return self.title
--- blog/comments/models.py
+++ blog/comments/models.py
@@ -5,7 +5,8 @@ class Comment(models.Model):
     text = models.TextField('Текст')
     article_id = models.IntegerField('Идентификатор статьи')
     parent_id = models.IntegerField('Идентификатор родительского комментария')
-    date = models.IntegerField('Дата публикации')
+    date = models.DecimalField(
+        'Дата публикации', max_digits=15, decimal_places=0)
 
     class Meta:
         verbose_name = 'Комментарий'
