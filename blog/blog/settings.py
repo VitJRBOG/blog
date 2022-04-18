f82c0fd88e75ed62fa109c1643fcb42c2b0fdf38
Vitaly Bogryakov <vitjrbog@icloud.com>
2022-04-18 09:14:53 +0500

    Update: described working with env variables

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import configparser
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,17 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-CONFIG_PATH = BASE_DIR.parent / 'config.ini'
-SECRET_KEY = ''
-if os.path.isfile(CONFIG_PATH):
-    config = configparser.ConfigParser()
-    config.read(CONFIG_PATH)
-    SECRET_KEY = config['Security']['SECRET_KEY']
-else:
-    raise FileNotFoundError(CONFIG_PATH)
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = []
 
